b51076ff445d53596c2e592513f68a9c54f15e32
bruno-calasans <brunogstsho@gmail.com>
2023-10-28 11:26:24 -0300

    create Home view

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import homeStyles from "./Home.style"
+
+function Home() {
+  return (
+    <View>
+      <Text>Home View</Text>
+    </View>
+  )
+}
+
+export default Home
