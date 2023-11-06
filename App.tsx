e56c87d97d7ff01b2cfd90a07257d90e3de75c25
bruno-calasans <brunogstsho@gmail.com>
2023-11-06 19:31:02 -0300

    add background image to app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, ImageBackground } from "react-native"
 import Button from "./src/components/Button/Button"
 import Box from "./src/components/Box/Box"
 import Group from "./src/components/Group/Group"
@@ -9,16 +9,29 @@ import { LinearGradient } from "expo-linear-gradient"
 
 export default function App() {
   return (
-    <LinearGradient colors={["#FF4E50", "#d3b51e"]} style={styles.container}>
-      <Box w="90%" bg="rgba(165, 138, 112, 0.253)" gap={10} pd={10}>
-        <View>
-          <NumberInput ph="Choose a number" c="white" onChange={console.log} />
-        </View>
-        <Box gap={5}>
-          <Button title="Reset" variant="filled" bg="purple" w="100%" />
-          <Button title="Confirm" variant="filled" bg="purple" w="100%" />
+    <LinearGradient colors={["#FF4E50", "#F9D423"]} style={styles.container}>
+      <ImageBackground
+        style={styles.content}
+        imageStyle={styles.bgImage}
+        source={{
+          uri: "https://raw.githubusercontent.com/academind/react-native-practical-guide-code/04-deep-dive-real-app/extra-files/images/background.png",
+        }}
+        resizeMode="cover" // default
+      >
+        <Box bg="rgba(165, 138, 112, 0.253)" w="90%" gap={10} pd={10}>
+          <View>
+            <NumberInput
+              ph="Choose a number"
+              c="white"
+              onChange={console.log}
+            />
+          </View>
+          <Box gap={5}>
+            <Button title="Reset" variant="filled" bg="purple" w="100%" />
+            <Button title="Confirm" variant="filled" bg="purple" w="100%" />
+          </Box>
         </Box>
-      </Box>
+      </ImageBackground>
     </LinearGradient>
   )
 }
@@ -27,8 +40,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "flex-start",
-    paddingTop: 100,
+  },
+  content: {
+    flex: 1,
+    paddingTop: 50,
+    alignItems: "center",
+  },
+  bgImage: {
+    opacity: 0.16,
   },
 })
